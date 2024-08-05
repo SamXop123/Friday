0b6b983bf900edced7a4f8774df92477fea0a567
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-08-05 20:06:08 +0530

    Add files via upload

---
+++ JARVIS/music_library.py
@@ -0,0 +1,22 @@
+
+music = {
+    "snowfall":"https://youtu.be/LlN8MPS7KQs?si=A2ZLHVil3s3fM-On",
+    "memory reboot": "https://youtu.be/mSMLdxRubPo?si=RWH0ZDmu64y12Z5o",
+    "after dark": "https://youtu.be/0KROgbD0kfQ?si=1hXl0tnLMyVNkkhu",
+    "resonance": "https://youtu.be/8GW6sLrK40k?si=_KPgyOdy04TtDql9",
+    "heat waves": "https://youtu.be/rfTgO9rpqck?si=pdQm6hs1FIuxDH3C",
+    "interstellar theme": "https://youtu.be/UDVtMYqUAyw?si=VCg-Za-rZ_D5Hsu_",
+    "no time for caution": "https://youtu.be/m3zvVGJrTP8?si=OuNeSieg00L9GDWk",
+    "cornfield chase": "https://youtu.be/JuSsvM8B4Jc?si=Qh2IldpA9_T-IyOz",
+    "metamorphosis": "https://youtu.be/NS9z2QHcZdY?si=H8qfiMQmZvMJu5fF",
+    "kompa passion": "https://youtu.be/fFyarF9DMcY?si=fts-nKoWVmk4OwyI",
+    "interlinked": "https://youtu.be/uCLbioejCWo?si=MUQGAA2vN1OLPjK9",
+    "fainted": "https://youtu.be/VoDAPYKWfJo?si=1ciq2Av_--4V4gfI",
+    "sweden": "https://youtu.be/aBkTkxKDduc?si=chdK41-pIqr2gKgS",
+    "aria math": "https://youtu.be/atgjKEgSqSU?si=uN0TeVk8ySi6VKRX",
+    "happy life fredji": "https://youtu.be/u4HJW2D9IVo?si=U_s1TZX0xmPKyJkJ",
+    "bloody mary best part": "https://youtu.be/mo5JTNRslTw?si=0MA1b3x-njchTcyW",
+    "bloody mary": "https://youtu.be/rOep6l519Ks?si=8Qy3jFe34BSNKnNY",
+    "faded": "https://youtu.be/60ItHLz5WEA?si=JBkKH6AYrfhJtglI",
+    "believer": "https://youtu.be/W0DM5lcj6mw?si=KUMTVWE4njDNIMvZ"
+}
