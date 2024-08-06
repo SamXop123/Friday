ac5320b1f2847fd47ca3b62b3afd9907760a7abb
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-08-06 15:29:47 +0530

    Update main.py

--- FRIDAY/main.py
+++ FRIDAY/main.py
@@ -3,6 +3,7 @@ import webbrowser
 import pyttsx3
 import music_library
 import requests
+import socket
 from openai import OpenAI
 from gtts import gTTS
 import pygame
@@ -48,6 +49,15 @@ def aiProcess(command):
     )
     return completion.choices[0].message.content
 
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+    
+def get_current_city():
+    response = requests.get('https://ipinfo.io/json')
+    data = response.json()
+    return data.get('city', 'Unknown')
 
 def processCommand(c):
     print(c)
@@ -75,6 +85,19 @@ def processCommand(c):
             speak(f"Sorry, I couldn't find the song {song}.")
         return
 
+    
+    if "my ip" in c.lower():
+        ip = get_ip_address()
+        print(f"Your IP address is {ip}")
+        speak(f"Your IP address is {ip}")
+    
+    if "weather" in c.lower():
+        city = get_current_city()
+        weather_info = get_weather(city)
+        print(weather_info)
+        speak(weather_info)
+
+    
     elif "news" in c.lower():
         r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}")
         if r.status_code == 200:
