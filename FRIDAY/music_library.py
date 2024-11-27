1859e2534ee039ab1ea986212dc164a1c4a9396b
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-11-27 18:34:17 +0530

    Update music_library.py

--- FRIDAY/music_library.py
+++ FRIDAY/music_library.py
@@ -3,15 +3,10 @@ music = {
     "snowfall":"https://youtu.be/LlN8MPS7KQs?si=A2ZLHVil3s3fM-On",
     "memory reboot": "https://youtu.be/mSMLdxRubPo?si=RWH0ZDmu64y12Z5o",
     "after dark": "https://youtu.be/0KROgbD0kfQ?si=1hXl0tnLMyVNkkhu",
-    "resonance": "https://youtu.be/8GW6sLrK40k?si=_KPgyOdy04TtDql9",
     "heat waves": "https://youtu.be/rfTgO9rpqck?si=pdQm6hs1FIuxDH3C",
-    "no time for caution": "https://youtu.be/m3zvVGJrTP8?si=OuNeSieg00L9GDWk",
-    "cornfield chase": "https://youtu.be/JuSsvM8B4Jc?si=Qh2IldpA9_T-IyOz",
-    "kompa passion": "https://youtu.be/fFyarF9DMcY?si=fts-nKoWVmk4OwyI",
     "interlinked": "https://youtu.be/uCLbioejCWo?si=MUQGAA2vN1OLPjK9",
-    "aria math": "https://youtu.be/atgjKEgSqSU?si=uN0TeVk8ySi6VKRX",
     "happy life fredji": "https://youtu.be/u4HJW2D9IVo?si=U_s1TZX0xmPKyJkJ",
-    "bloody mary": "https://youtu.be/mo5JTNRslTw?si=0MA1b3x-njchTcyW",
     "faded": "https://youtu.be/60ItHLz5WEA?si=JBkKH6AYrfhJtglI",
     "believer": "https://youtu.be/W0DM5lcj6mw?si=KUMTVWE4njDNIMvZ"
+    # Add more here...
 }
