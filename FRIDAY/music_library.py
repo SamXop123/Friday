79fd40265a80766c9d4b366b7589374be630441d
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-12-22 18:35:54 +0530

    Update music_library.py

--- FRIDAY/music_library.py
+++ FRIDAY/music_library.py
@@ -5,8 +5,9 @@ music = {
     "after dark": "https://youtu.be/0KROgbD0kfQ?si=1hXl0tnLMyVNkkhu",
     "heat waves": "https://youtu.be/rfTgO9rpqck?si=pdQm6hs1FIuxDH3C",
     "interlinked": "https://youtu.be/uCLbioejCWo?si=MUQGAA2vN1OLPjK9",
+    "aria math": "https://youtu.be/atgjKEgSqSU?si=tZ6DUl6vUEIdDSYr",
     "happy life fredji": "https://youtu.be/u4HJW2D9IVo?si=U_s1TZX0xmPKyJkJ",
+    "copines": "https://youtu.be/JXijFYXK_44?si=YiX9OnTUFFc99-Ys",
     "faded": "https://youtu.be/60ItHLz5WEA?si=JBkKH6AYrfhJtglI",
-    "believer": "https://youtu.be/W0DM5lcj6mw?si=KUMTVWE4njDNIMvZ"
     # Add more here...
 }
